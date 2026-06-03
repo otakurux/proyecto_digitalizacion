8510f5b13b17e45c94bfa62d9f35356cd5eaaeae
otakurux <mamanideivid33@gmail.com>
2026-06-03 15:23:21 -0400

    chore: configuracion de despliegue

--- run.py
+++ run.py
@@ -1,16 +1,11 @@
 from app import create_app
 import logging
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    # Silenciar logs de peticiones HTTP (reduce ruido de Unity WebGL)
     log = logging.getLogger('werkzeug')
-    log.setLevel(logging.DEBUG)
-
-    print("🏛️  UMSA Digital iniciado")
-    print("📍 http://localhost:5000")
-    print("🎮 Animación Unity: http://localhost:5000/animacion/")
-    print("=" * 50)
-
-    app.run(debug=False, port=5000, use_reloader=False)
+    log.setLevel(logging.ERROR)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
